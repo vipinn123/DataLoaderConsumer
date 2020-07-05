032c7716a513093748f2b3c23019fd20ab757a99
vipinn123 <vipinn@gmail.com>
2020-07-05 12:00:31 +0530

    mongo db storage

--- src/main/java/com/spring/trade/entities/ZerodhaTickDepth.java
+++ src/main/java/com/spring/trade/entities/ZerodhaTickDepth.java
@@ -6,16 +6,16 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
-@Entity // This tells Hibernate to make a table out of this class
-@Table(name = "zerodha_tick_depth")
+//@Entity // This tells Hibernate to make a table out of this class
+//@Table(name = "zerodha_tick_depth")
 public class ZerodhaTickDepth {
 	
 	int orders;
 	int quantity;
 	double price;
 	
-	@ManyToOne(fetch = FetchType.LAZY, optional = false)
-	@JoinColumn(name = "tick_id", nullable = false)	  
+	//@ManyToOne(fetch = FetchType.LAZY, optional = false)
+	//@JoinColumn(name = "tick_id", nullable = false)	  
 	private ZerodhaTick tick;
 
 	public ZerodhaTickDepth() {
